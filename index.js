8c8094f4f5617963e6972469cd770d7ac2ff4127
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-13 18:23:54 +0100

    update
    updated files

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-// Arrays - ordered lists of items - coposite / complex data type
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard] //array
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
 
-let featuredPosts = [
-    "Check out my netflix",
-    "Here's the code for my project",
-    "I've relaunched my portfolio"
-]
+// Create a new function called startGame() that calls renderGame()
+function startGame() {
+    renderGame();
+}
 
-// console.log(featuredPosts[2])
+function renderGame() {
+    // 3. Render the cards on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
+    sumEl.textContent = `Sum: ${sum}`
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
 
-console.log(featuredPosts.length)
-
-// Create an array that describes yourself. Use the three primitive data types you've learned
-// It should contain your name (string), your age (number), and whether you like pizza (boolean)
-let mySelf = ["peculiar", 23, true]
-
-// Array .push() and .pop()
-let cards = [7, 4]
-cards.push(6)
-
-let messages = [
-    "Hey, how's it going?",
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately."
-]
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 8
+    // 2. Add the new card to the sum variable
+    sum += card
+    // 3. Call startGame()
+    renderGame()
+}
