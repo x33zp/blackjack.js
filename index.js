71d262d2f50440d2944af86549683a81b746a315
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-13 19:09:03 +0100

    update
    push te updated code before i head home, to continue when i get home

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard] //array
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-// 2. Store the cards paragraph in a variable called cardsEl
-let cardsEl = document.getElementById("cards-el")
+///
+// for loop
+// Count to ten!
 
-// Create a new function called startGame() that calls renderGame()
-function startGame() {
-    renderGame();
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+
+//    START           FINISH       STEP SIZE
+for (let count = 1; count < 21; count += 1) {
+
+    console.log(count)
+
 }
 
-function renderGame() {
-    // 3. Render the cards on the page using this format -> "Cards: 10 4"
-    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
-    sumEl.textContent = `Sum: ${sum}`
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-}
-
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
-function newCard() {
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 8
-    // 2. Add the new card to the sum variable
-    sum += card
-    // 3. Call startGame()
-    renderGame()
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+for (let num = 10; num < 101; num += 10) {
+    console.log(num);
 }
--- notes.js
+++ notes.js
@@ -164,9 +164,46 @@ function renderGame() {
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
 function newCard() {
     // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 8
+    let card = 6
     // 2. Add the new card to the sum variable
     sum += card
+    // Push the card to the cards array
+    cards.push(card);
     // 3. Call startGame()
+    console.log(cards)
     renderGame()
 }
+
+// Arrays - ordered lists of items - coposite / complex data type
+
+let featuredPosts = [
+    "Check out my netflix",
+    "Here's the code for my project",
+    "I've relaunched my portfolio"
+]
+
+// console.log(featuredPosts[2])
+
+console.log(featuredPosts.length)
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+let mySelf = ["peculiar", 23, true]
+
+// Array .push() and .pop()
+let cards = [7, 4]
+cards.push(6)
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+console.log(messages)
+
+// removing the last message of an array
+messages.pop()
+console.log(messages)
