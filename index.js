8ee4197c06a9112f81600e59d8c6c00667d0916c
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-16 13:36:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,85 @@
-///
-// Objects - store data in-depth - composite / complex data type
-// key-value pairs
+// let firstCard = getRandomCard()
+// let secondCard = getRandomCard()
 
-let course = {
-    title: "Learn CSS Grid For Free",
-    lessons: 16,
-    creator: "Per Harald Borgen",
-    length: 63,
-    level: 2,
-    isFree: true,
-    tags: ["html", "css"]
+let player = {
+    name: "zubby",
+    chips: 200
+}
+let cards = [] //array
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = `${player.name}: $${player.chips}`
+
+
+// console.log(cards)
+
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
-console.log(course.tags)
-
-// Create an object that represents an airbnb castle listing.
-// It should contain at least one boolean, one string, one number, and one array
-// Log out at least two of the keys using the dot notation
-
-let castle = {
-    title: "Live liek a king",
-    price: 200,
-    isSuperHost: true,
-    images: ["img/castle1.png", "img/caste2.png"]
+// Create a new function called startGame() that calls renderGame()
+function startGame() {
+    isAlive = true
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame();
 }
 
-console.log(castle.price)
-console.log(castle.isSuperHost)
+function renderGame() {
+    // 3. Render the cards on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: "
+    // Create a for loop that renders out all the cards instead of just two
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += `${cards[i]} `
+    }
+
+    sumEl.textContent = `Sum: ${sum}`
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack == false) {
+        // 1. Create a card variable, and hard code its value to a number (2-11)
+        let card = getRandomCard()
+        // 2. Add the new card to the sum variable
+        sum += card
+        // Push the card to the cards array
+        cards.push(card);
+        // 3. Call startGame()
+        console.log(cards)
+        renderGame()
+    }
+}
