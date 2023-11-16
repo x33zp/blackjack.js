f3d27d96b5b75f6e2a7f00384696e55c9ad83304
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-16 13:08:27 +0100

    logical operators
    getting new card only if is alive = true and has black jack = false

--- notes.js
+++ notes.js
@@ -352,3 +352,51 @@ function rollDice() {
 }
 
 console.log(rollDice())
+
+///
+// logical operators
+// AND & OR operators
+let hasCompletedCourse = true
+let givesCertificate = true
+
+// nested if runs the c0de too
+// if (hasCompletedCourse === true) {
+//     if (givesCertificate === true) {
+
+//     }
+// }
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating certificate....")
+}
+
+//
+let hasSolvedChallenge = false
+let hasHintsLeft = false
+
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution()
+}
+
+function showSolution() {
+    console.log("Showing the solution....")
+}
+
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+let likesDocumentaries = true
+let likesStartups = false
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
