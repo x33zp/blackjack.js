7f2fc87f87a6a9242e91d9eafd019ad29af2c62f
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-16 13:31:05 +0100

    objects

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-//
+///
 // Objects - store data in-depth - composite / complex data type
 // key-value pairs
+
+let course = {
+    title: "Learn CSS Grid For Free",
+    lessons: 16,
+    creator: "Per Harald Borgen",
+    length: 63,
+    level: 2,
+    isFree: true,
+    tags: ["html", "css"]
+}
+
+console.log(course.tags)
+
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let castle = {
+    title: "Live liek a king",
+    price: 200,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/caste2.png"]
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)
